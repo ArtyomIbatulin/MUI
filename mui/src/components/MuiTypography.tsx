b21077973db5f94726531b984fb2e065c8ae683d
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2022-05-31 18:22:57 +0300

    fixed typography component

--- mui/src/components/MuiTypography.tsx
+++ mui/src/components/MuiTypography.tsx
@@ -6,9 +6,15 @@ export const MuiTypography = () => {
         <Typography variant="h1">h1 headers</Typography>
         <Typography variant="h2">h2 headers</Typography>
         <Typography variant="h3">h3 headers</Typography>
-        <Typography variant="h4">h4 headers</Typography>
+        <Typography variant="h4" component='h1' gutterBottom>h4 headers</Typography>
         <Typography variant="h5">h5 headers</Typography>
         <Typography variant="h6">h6 headers</Typography>
+
+        <Typography variant="subtitle1">subtitle1</Typography>
+        <Typography variant="subtitle2">subtitle2</Typography>
+
+        <Typography variant="body1">body1</Typography>
+        <Typography variant="body2">body2</Typography>
     </div>
   )
 }
