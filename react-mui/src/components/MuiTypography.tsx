989d59efc49e9037de5697d57da17d69cd0347a4
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2022-05-31 16:39:11 +0300

    veiw typography

--- react-mui/src/components/MuiTypography.tsx
+++ react-mui/src/components/MuiTypography.tsx
@@ -3,7 +3,12 @@ import { Typography } from "@mui/material"
 export const MuiTypography = () => {
   return (
     <div>
-      <Typography></Typography>
+      <Typography variant="h1">h1 headers</Typography>
+      <Typography variant="h2">h2 headers</Typography>
+      <Typography variant="h3">h3 headers</Typography>
+      <Typography variant="h4">h4 headers</Typography>
+      <Typography variant="h5">h5 headers</Typography>
+      <Typography variant="h6">h6 headers</Typography>
     </div>
   )
 }
