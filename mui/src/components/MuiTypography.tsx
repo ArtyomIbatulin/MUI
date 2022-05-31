460e56ba7c3c5f1a0426e8c456101a6c9394cafb
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2022-05-31 17:56:02 +0300

    added typography headers

---
+++ mui/src/components/MuiTypography.tsx
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material"
+
+export const MuiTypography = () => {
+  return (
+    <div>
+        <Typography variant="h1">h1 headers</Typography>
+        <Typography variant="h2">h2 headers</Typography>
+        <Typography variant="h3">h3 headers</Typography>
+        <Typography variant="h4">h4 headers</Typography>
+        <Typography variant="h5">h5 headers</Typography>
+        <Typography variant="h6">h6 headers</Typography>
+    </div>
+  )
+}
