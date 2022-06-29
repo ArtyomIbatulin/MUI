711d062d3276df0a7a0031132134da6a9de41440
ArtyomIbatulin <artem.alekseevich@bk.ru>
2022-06-29 19:12:19 +0300

    added autocomplete all varibales

--- mui/src/App.tsx
+++ mui/src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css'
-import MuiInpAdornments from './components/MuiInpAdornments'
-import { MuiTextField } from './components/MuiTextField'
+import MuiAnInputs from './components/MuiAnInputs'
+import MuiAutocmpl from './components/MuiAutocmpl'
+// import MuiInpAdornments from './components/MuiInpAdornments'
+// import { MuiTextField } from './components/MuiTextField'
 // import { MuiButton } from './components/MuiButton'
 // import { MuiTypography } from './components/MuiTypography'
 
@@ -10,8 +12,10 @@ function App() {
     <div className="App">
       {/* <MuiTypography></MuiTypography> */}
       {/* <MuiButton></MuiButton> */}
-      <MuiTextField></MuiTextField>
-      <MuiInpAdornments></MuiInpAdornments>
+      {/* <MuiTextField></MuiTextField> */}
+      {/* <MuiInpAdornments></MuiInpAdornments> */}
+      <MuiAnInputs></MuiAnInputs>
+      <MuiAutocmpl></MuiAutocmpl>
     </div>
   )
 }
---
+++ mui/src/components/MuiAnInputs.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const MuiAnInputs = () => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default MuiAnInputs
