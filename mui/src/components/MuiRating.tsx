112748ed0a1d22ad25abee403480fc67ca742f44
ArtyomIbatulin <artem.alekseevich@bk.ru>
2022-06-30 21:17:27 +0300

    fixed rating plus precisiion

--- mui/src/components/MuiRating.tsx
+++ mui/src/components/MuiRating.tsx
@@ -10,23 +10,25 @@ const MuiRating = () => {
     <Box
       sx={{
         "& > legend": { mt: 2 },
-        pt: 5, pl: 20 
+        pt: 5,
+        pl: 20,
       }}
     >
       <Typography component="legend">Controlled</Typography>
       <Rating
         name="simple-controlled"
+        precision={0.5}
         value={value}
         onChange={(event, newValue) => {
           setValue(newValue);
         }}
       />
       <Typography component="legend">Read only</Typography>
-      <Rating name="read-only" value={value} readOnly />
+      <Rating name="read-only" value={value} readOnly precision={0.5} />
       <Typography component="legend">Disabled</Typography>
-      <Rating name="disabled" value={value} disabled />
+      <Rating name="disabled" value={value} disabled precision={0.5} />
       <Typography component="legend">No rating given</Typography>
-      <Rating name="no-value" value={null} />
+      <Rating name="no-value" value={null} precision={0.5} />
     </Box>
   );
 };
