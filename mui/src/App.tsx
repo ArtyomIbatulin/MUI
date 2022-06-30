1a62b7fca24f393cb3e22b092b28edbbf9b83fa5
ArtyomIbatulin <artem.alekseevich@bk.ru>
2022-06-30 19:21:04 +0300

    fixed autocomplet add films dbe

--- mui/src/App.tsx
+++ mui/src/App.tsx
@@ -1,27 +1,26 @@
-import './App.css'
-import MuiGroupedAutocomp from './components/MuiGroupedAutocomp'
+import "./App.css";
+import MuiGroupedAutocomp from "./components/autoComplete/MuiGroupedAutocomp";
 // import MuiAnInputs from './components/MuiAnInputs'
-// import MuiAutocmpl from './components/MuiAutocmpl' 
-import MuiSelContries from './components/MuiSelContries'
+// import MuiAutocmpl from "./components/autoComplete/MuiAutocmpl";
+import MuiSelContries from "./components/autoComplete/MuiSelContries";
 // import MuiInpAdornments from './components/MuiInpAdornments'
 // import { MuiTextField } from './components/MuiTextField'
 // import { MuiButton } from './components/MuiButton'
 // import { MuiTypography } from './components/MuiTypography'
 
 function App() {
-
   return (
     <div className="App">
       {/* <MuiTypography></MuiTypography> */}
       {/* <MuiButton></MuiButton> */}
       {/* <MuiTextField></MuiTextField> */}
       {/* <MuiInpAdornments></MuiInpAdornments> */}
+      {/* <MuiAnInputs></MuiAnInputs> */}
       {/* <MuiAutocmpl></MuiAutocmpl> */}
       <MuiSelContries></MuiSelContries>
-      {/* <MuiAnInputs></MuiAnInputs> */}
       <MuiGroupedAutocomp></MuiGroupedAutocomp>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
