0833293e92e59ea9bb8029388ee7b436b91485d6
ArtyomIbatulin <artem.alekseevich@bk.ru>
2022-06-30 19:55:34 +0300

    fixed autocomplete add dis opt

--- mui/src/components/autoComplete/MuiGroupedAutocomp.tsx
+++ mui/src/components/autoComplete/MuiGroupedAutocomp.tsx
@@ -11,19 +11,40 @@ const MuiGroupedAutocomp = () => {
     };
   });
 
+  // One time slot every 30 minutes.
+  const timeSlots = Array.from(new Array(24 * 2)).map(
+    (_, index) =>
+      `${index < 20 ? "0" : ""}${Math.floor(index / 2)}:${
+        index % 2 === 0 ? "00" : "30"
+      }`
+  );
+
   return (
-    <Autocomplete
-      id="grouped-demo"
-      options={options.sort(
-        (a, b) => -b.firstLetter.localeCompare(a.firstLetter)
-      )}
-      groupBy={(option) => option.firstLetter}
-      getOptionLabel={(option) => option.title}
-      sx={{ width: 350, pt: 5, pl: 20 }}
-      renderInput={(params) => (
-        <TextField {...params} label="With categories" />
-      )}
-    />
+    <>
+      <Autocomplete
+        id="grouped-demo"
+        options={options.sort(
+          (a, b) => -b.firstLetter.localeCompare(a.firstLetter)
+        )}
+        groupBy={(option) => option.firstLetter}
+        getOptionLabel={(option) => option.title}
+        sx={{ width: 350, pt: 5, pl: 20 }}
+        renderInput={(params) => (
+          <TextField {...params} label="With categories" />
+        )}
+      />
+      <Autocomplete
+        id="disabled-options-demo"
+        options={timeSlots}
+        getOptionDisabled={(option) =>
+          option === timeSlots[0] || option === timeSlots[2]
+        }
+        sx={{ width: 350, pt: 5, pl: 20 }}
+        renderInput={(params) => (
+          <TextField {...params} label="Disabled options" />
+        )}
+      />
+    </>
   );
 };
 
