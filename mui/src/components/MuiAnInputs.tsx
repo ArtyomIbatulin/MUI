d5836fe1f740cf70a61e8e1be8d5b10f3570ba74
ArtyomIbatulin <artem.alekseevich@bk.ru>
2022-06-30 18:09:48 +0300

    fixed chekbox icon

--- mui/src/components/MuiAnInputs.tsx
+++ mui/src/components/MuiAnInputs.tsx
@@ -1,27 +1,46 @@
 import { pink } from "@mui/material/colors";
 import Checkbox from "@mui/material/Checkbox";
 import Stack from "@mui/material/Stack";
+import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
+import Favorite from "@mui/icons-material/Favorite";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
 
 const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 const MuiAnInputs = () => {
   return (
-    <Stack direction="row" sx={{ pt: 3, pl: 20 }}>
-      <Checkbox {...label} defaultChecked />
-      <Checkbox {...label} defaultChecked color="secondary" />
-      <Checkbox {...label} defaultChecked color="success" />
-      <Checkbox {...label} defaultChecked color="default" />
-      <Checkbox
-        {...label}
-        defaultChecked
-        sx={{
-          color: pink[800],
-          "&.Mui-checked": {
-            color: pink[600],
-          },
-        }}
-      />
-    </Stack>
+    <>
+      <Stack direction="row" sx={{ pt: 3, pl: 20 }}>
+        <Checkbox {...label} defaultChecked />
+        <Checkbox {...label} defaultChecked color="secondary" />
+        <Checkbox {...label} defaultChecked color="success" />
+        <Checkbox {...label} defaultChecked color="default" />
+        <Checkbox
+          {...label}
+          defaultChecked
+          sx={{
+            color: pink[800],
+            "&.Mui-checked": {
+              color: pink[600],
+            },
+          }}
+        />
+      </Stack>
+
+      <Stack direction="row"  sx={{ pt: 3, pl: 20 }}>
+        <Checkbox
+          {...label}
+          icon={<FavoriteBorder />}
+          checkedIcon={<Favorite />}
+        />
+        <Checkbox
+          {...label}
+          icon={<BookmarkBorderIcon />}
+          checkedIcon={<BookmarkIcon />}
+        />
+      </Stack>
+    </>
   );
 };
 
